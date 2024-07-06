4e2165fdb2c812ed190ba0a504ae8d176a29a20a
Brayan Edgar Blanco Camacho <brayanbrx1@gmail.com>
2024-07-06 00:41:53 -0400

    Colecciones

---
+++ src/P/Colecciones/CuentasUsuarios.java
@@ -0,0 +1,28 @@
+package P.Colecciones;
+import java.util.*;
+
+
+/**
+ *
+ * @author braya
+ */
+public class CuentasUsuarios {
+    public static void main(String[] args) {
+        Cliente cl1= new Cliente("Diego Perez","00001",200000);
+        Cliente cl2= new Cliente("Rafael Nadal","00002",250000);
+        Cliente cl3= new Cliente("Penelope Cruz","00003",300000);
+        Cliente cl4= new Cliente("Julio Iglesias","00004",500000);
+        
+        Set <Cliente>clientesBanco = new HashSet<Cliente>();
+        
+        clientesBanco.add(cl1);
+        clientesBanco.add(cl2);
+        clientesBanco.add(cl3);
+        clientesBanco.add(cl4);
+        
+        for(Cliente e:clientesBanco){
+            System.out.println(e);
+        }
+    }
+    
+}
